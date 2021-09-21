77c2a265eb90891184b1a28af74ed81d0cddabaa
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-09-20 19:34:23 -0700

    city data posting

--- src/App.js
+++ src/App.js
@@ -1,1 +1,43 @@
 import { Component } from "react";
+import LocationForm from './components/LocationForm'
+import City from './components/City'
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      searchQuery: '',
+      location: {},
+      error: false
+    } 
+  }
+
+
+  handleChange = (userSearchQuery) =>{
+    this.setState({
+      searchQuery: userSearchQuery
+    })
+  }
+  
+  handleSubmit = (location) =>{
+    this.setState({
+      location: location
+    });
+  }
+
+  render(){
+    
+    return(
+      <>
+      <LocationForm searchQuery={this.state.searchQuery} handleSubmit={this.handleSubmit} update={this.handleChange}/>
+      <City locationUpdate={this.state.location} />
+      </>
+    );
+  }
+  
+  
+}
+
+
+export default App;
